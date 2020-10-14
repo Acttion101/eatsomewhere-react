5dfd04aeccc1d702141a6451f0bd68ed3332ff04
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-15 01:43:45 +0700

    new-Typography

---
+++ src/components/Typography.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+ export const Text = styled.h1`
+  width: 70%;
+  display: flex;
+  align-items: flex-start;
+  padding:1rem;
+ `
+export const Logo = styled.div`
+padding: 1rem;`
