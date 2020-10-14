d69543de2c1ad2de2eafa3a75bc4c292900fe6f2
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-15 01:46:56 +0700

    new-pages/Register

---
+++ src/pages/Register.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Register/Navbar'
+import Content from '../components/Register/Content'
+
+function Register() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Content />
+    </div>
+  )
+}
+
+export default Register
