eb212d7aac996baaaf4dca055b9e78c1c7cf664d
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-15 01:39:25 +0700

    edit-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import "./index.css";
-import Navbar from "./components/Navbar"
-import ContainerLayout from "./components/ContainerLayout"
-// import "./sass/main.scss"
+import React, { Suspense, lazy } from 'react'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import GlobalStyle from './components/GlobalStyle'
+
+
+const Login = lazy(() => import('./pages/Login'))
+const Home = lazy(() => import('./pages/Home'))
+const Register = lazy(() => import('./pages/Register'))
 function App() {
   return (
-    <ContainerLayout>
-     <Navbar />
-    </ContainerLayout>
-  );
+    <>
+    <GlobalStyle/>
+    <Router>
+      <Suspense fallback="...Loading">
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/register" exact component={Register} />
+        </Switch>
+      </Suspense>
+    </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
