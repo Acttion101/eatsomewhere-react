dee52eebc51b58f73daebf88964de4d9690c8136
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-15 16:11:43 +0700

    new-Home/content

---
+++ src/components/Home/Content/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Content() {
+  return (
+    <div className="content-container">
+      <div>img</div>
+      <div className="content-input">
+        <input></input>
+      </div>
+    </div>
+  )
+}
+
+export default Content
