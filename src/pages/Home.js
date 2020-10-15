8b860ab6e51eae39ea3e133240f75626e0c61e7a
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-15 16:12:21 +0700

    new-Home/pages

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Home/Navbar'
+import Content from '../components/Home/Content'
+
+function Home() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Content />
+    </div>
+  )
+}
+
+export default Home
