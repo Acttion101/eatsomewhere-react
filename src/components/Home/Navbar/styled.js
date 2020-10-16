760e1b98bee4b2008489a55be47892ab60d23f3e
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-16 12:35:50 +0700

    new-Home/Navbar/styled

---
+++ src/components/Home/Navbar/styled.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+
+export const NavBarContainer = styled.div`
+  background-color: #FF7700;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  height: 60px;
+  box-shadow: 0px 10px #888888;
+ 
+`
+
+
+export const NavButtonLogin = styled.button`
+  padding: 0.5rem 1rem;
+  border: #707070 solid 2px;
+  border: none;
+  background-color: #ffffff;
+  border-radius: 3rem;
+  cursor: pointer;
+  box-shadow: 0px 5px #00000025;
+  font-size: 20px;
+  position: relative;
+  transition: 0.5s;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
+
+
+
+export const ActionContainer = styled.ul`
+   display: flex;
+   flex: 1;
+   justify-content: flex-end;
+   align-items:center;
+ `
+  export const Action = styled.li`
+   padding: 0 1rem;
+
+ `
+
+
+
+
+
