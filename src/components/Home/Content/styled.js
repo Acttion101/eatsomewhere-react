1a7f816ec2c9c9cd5edf2aef8e0d99185bdf40ea
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-16 12:35:09 +0700

    new-Home/styled

---
+++ src/components/Home/Content/styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ContainerHome = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #bf804b;
+`
+export const ContentHome = styled.div`
+`
+export const ContentImage = styled.div`
+
+  height: 100vh;
+  width: 100vw;
+  background-image: url(Promotion.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+
+  position: relative;
+`
