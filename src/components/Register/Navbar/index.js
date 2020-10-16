0ec67f887cc444225ce43545261b4a10a5dcf4cd
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-16 12:39:02 +0700

    edit-Register/Content,Navbar

--- src/components/Register/Navbar/index.js
+++ src/components/Register/Navbar/index.js
@@ -1,38 +1,22 @@
 import React from 'react'
-import styled from 'styled-components'
+import { NavContainerRegister, NavButtonRegister } from './styled'
 import {
   Text,
   Logo
 } from '/Users/ramita/eatsomewhere-react/src/components/Typography'
+import { Link } from 'react-router-dom'
 
-const NavContainerRegister = styled.div`
-  background-color: #f37305;
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-  height: 60px;
-  box-shadow: 0px 5px #00000025;
-`
-
-const NavButtonRegister = styled.button`
-  padding: 1rem 2rem;
-  border: #707070 solid 2px;
-  border: none;
-  background-color: #ffffff;
-  border-radius: 3rem;
-  cursor: pointer;
-  box-shadow: 0px 5px #00000025;
-  font-size: 20px;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-`
 function Navbar() {
   return (
     <NavContainerRegister>
-      <Logo>Logo</Logo>
-      <Text>WHERE TO EAT</Text>
-      <NavButtonRegister>เข้าสู่ระบบ</NavButtonRegister>
+      <Logo>
+        <Link to="/home">Logo</Link>
+        <Text>WHERE TO EAT</Text>
+      </Logo>
+
+      <NavButtonRegister>
+        <Link to="/login">เข้าสู่ระบบ</Link>
+      </NavButtonRegister>
     </NavContainerRegister>
   )
 }
