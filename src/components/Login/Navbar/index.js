e6dfbff3de6ca24720d70eab012267fc6b034af4
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-16 12:37:58 +0700

    edit&new/styled-Login/Navbar

--- src/components/Login/Navbar/index.js
+++ src/components/Login/Navbar/index.js
@@ -1,39 +1,22 @@
 import React from 'react'
-import styled from 'styled-components'
+import { NavContainerLogin, NavButtonLogin } from './styled'
+import { Link } from 'react-router-dom'
 import {
   Text,
   Logo
 } from '/Users/ramita/eatsomewhere-react/src/components/Typography'
 
-const NavContainerLogin = styled.div`
-  background-color: #f37305;
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-  height: 60px;
-  box-shadow: 0px 5px #00000025;
-`
-
-const NavButtonLogin = styled.button`
-  padding: 1rem 2rem;
-  border: #707070 solid 2px;
-  border: none;
-  background-color: #ffffff;
-  border-radius: 3rem;
-  cursor: pointer;
-  box-shadow: 0px 5px #00000025;
-  font-size: 20px;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-`
 function Navbar() {
-  
   return (
     <NavContainerLogin>
-      <Logo>Logo</Logo>
-      <Text>WHERE TO EAT</Text>
-      <NavButtonLogin>สมัครสมาชิก</NavButtonLogin>
+      <Logo>
+        <Link to="/home">Logo</Link>
+        <Text>WHERE TO EAT</Text>
+      </Logo>
+
+      <NavButtonLogin>
+        <Link to="/register">สมัครสมาชิก</Link>
+      </NavButtonLogin>
     </NavContainerLogin>
   )
 }
