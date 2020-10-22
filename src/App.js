84564fb7c4e6ee5a2d2631eb9d4450ff0a6f251f
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-22 23:10:04 +0700

    edit-app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Footer } from './components/Footer/styled'
 import GlobalStyle from './components/GlobalStyle'
-
+import { TextFooter } from '/Users/ramita/eatsomewhere-react/src/components/Typography'
 
 const Login = lazy(() => import('./pages/Login'))
 const Home = lazy(() => import('./pages/Home'))
@@ -12,18 +13,19 @@ const Register = lazy(() => import('./pages/Register'))
 function App() {
   return (
     <>
-    <GlobalStyle/>
-    <Router>
-      <Suspense fallback="...Loading">
-        <Switch>
-          <Route path="/login" exact component={Login} />
-          <Route path="/home" exact component={Home} />
-          <Route path="/register" exact component={Register} />
-          {/* <Route path="/news" exact component={News} />
+      <GlobalStyle />
+      <Router>
+        <Suspense fallback="...Loading">
+          <Switch>
+            <Route path="/login" exact component={Login} />
+            <Route path="/home" exact component={Home} />
+            <Route path="/register" exact component={Register} />
+            {/* <Route path="/news" exact component={News} />
           <Route path="/community" exact component={Community} /> */}
-        </Switch>
-      </Suspense>
-    </Router>
+          </Switch>
+          <Footer ><TextFooter>Copy right by where to eat</TextFooter></Footer>
+        </Suspense>
+      </Router>
     </>
   )
 }
