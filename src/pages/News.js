c688c343c519eda765be3277f6573bc6a7479472
Acttion101 <pongponpan102@gmail.com>
2020-10-22 15:59:25 +0700

    new-Pages/News

---
+++ src/pages/News.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../components/Login/Navbar"
+// import Content from "../components/Login/Content"
+
+function News() {
+  return (
+    <div>
+      <Navbar />
+      
+    </div>
+  )
+}
+export default News
