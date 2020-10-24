22078e19a0d9aa9aff71b83fffa07db22d98378b
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-24 19:52:58 +0700

    edit-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import routes from './config/routes'
 import { Footer } from './components/Footer/styled'
 import GlobalStyle from './components/GlobalStyle'
 import { TextFooter } from '/Users/ramita/eatsomewhere-react/src/components/Typography'
 
-const Login = lazy(() => import('./pages/Login'))
-const Home = lazy(() => import('./pages/Home'))
-const Register = lazy(() => import('./pages/Register'))
-// const News = lazy(() => import('./pages/News'))
-// const Community = lazy(() => import('./pages/Community'))
+
 
 function App() {
   return (
@@ -17,11 +14,9 @@ function App() {
       <Router>
         <Suspense fallback="...Loading">
           <Switch>
-            <Route path="/login" exact component={Login} />
-            <Route path="/home" exact component={Home} />
-            <Route path="/register" exact component={Register} />
-            {/* <Route path="/news" exact component={News} />
-          <Route path="/community" exact component={Community} /> */}
+          {Object.keys(routes).map(routeKey => (
+              <Route key={routeKey} {...routes[routeKey]} />
+            ))}
           </Switch>
           <Footer ><TextFooter>Copy right by where to eat</TextFooter></Footer>
         </Suspense>
