05338b10846d8df6a4633092e6bd15d8929f2d5f
Acttion101 <pongponpan102@gmail.com>
2020-10-24 21:59:30 +0700

    edit-styled/News

--- src/components/News/Content/styled.js
+++ src/components/News/Content/styled.js
@@ -5,18 +5,21 @@ export const BackContent = styled.div`
   margin: 50px 0 0 50px;
   padding: 10px;
   border-radius: 2rem;
-  justify-content: center;
   align-items: top;
   height: 100vh;
   width: 70vw;
   background-color: #ffffff;
+  flex-direction: column;
 `
 export const Container = styled.div`
   padding: 2px 16px;
   
 `
 export const Card = styled.div`
-
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
+  width: 1200px;
+  height: 300px;
+  &:hover{
+    box-shadow: 0 8px 30px 0 rgba(0,0,0,0.2);
+  }
 `
