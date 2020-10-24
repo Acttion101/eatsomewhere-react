33f0d120bdd9d4efe80758f2923978affddd0731
Acttion101 <pongponpan102@gmail.com>
2020-10-24 21:58:18 +0700

    edit-News/content

--- src/components/News/Content/index.js
+++ src/components/News/Content/index.js
@@ -1,21 +1,39 @@
 import React from 'react'
-import { BackContent, Container,Card } from './styled'
+import { BackContent, Container, Card,} from './styled'
 
 function index() {
   return (
     <BackContent>
       <h1>ข่าวสารและกิจกรรม</h1>
       <Card>
-      
-      <Container>
-        <h4>
-          <b>John Doe</b>
-        </h4>
-        <p>Architect & Engineer</p>
-
-       </Container>  
+        <img src ="../aseets/pic2.jpg" />
+        <Container>
+          <h4>
+            <b>โปรโมชั่นร้าน นิมานคาเฟ่</b>
+          </h4>
+          <p>โปรโมชั่น 1 แถม 1 ร้านนิมานคาเฟ่</p>
+        </Container>
       </Card>
-     
+
+      <Card>
+        <img src ="../aseets/pic2.jpg" />
+        <Container>
+          <h4>
+            <b>โปรโมชั่นร้าน นิมานคาเฟ่</b>
+          </h4>
+          <p>โปรโมชั่น 1 แถม 1 ร้านนิมานคาเฟ่</p>
+        </Container>
+      </Card>
+
+      <Card>
+        <img src ="../aseets/pic2.jpg" />
+        <Container>
+          <h4>
+            <b>โปรโมชั่นร้าน นิมานคาเฟ่</b>
+          </h4>
+          <p>โปรโมชั่น 1 แถม 1 ร้านนิมานคาเฟ่</p>
+        </Container>
+      </Card>
     </BackContent>
   )
 }
