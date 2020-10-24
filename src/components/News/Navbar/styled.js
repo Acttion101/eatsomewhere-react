483c8296a344d93d59f61aca0fdcf642f205310f
Acttion101 <pongponpan102@gmail.com>
2020-10-24 19:25:37 +0700

    edit-news/navbar

--- src/components/News/Navbar/styled.js
+++ src/components/News/Navbar/styled.js
@@ -7,6 +7,7 @@ export const NavContainerLogin = styled.div`
   padding: 10px;
   height: 60px;
   box-shadow: 0px 5px #00000025;
+  width:100%;
 `
 
 export const NavButtonLogin = styled.button`
@@ -38,4 +39,24 @@ export const NavButtonHome = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
+`
+export const NavButtonNews = styled.button`
+  padding: 0.5rem 1rem;
+  border: #707070 solid 2px;
+  border: none;
+  background-color: #ffffff;
+  border-radius: 3rem;
+  cursor: pointer;
+  box-shadow: 0px 5px #00000025;
+  font-size: 20px;
+  position: relative;
+  transition: 0.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Hello = styled.div`
+  display: flex;
+  flex-direction: row;
 `
