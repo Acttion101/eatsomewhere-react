3b0094a1de5c6ac8a72bf584b48a152aee772bca
Acttion101 <pongponpan102@gmail.com>
2020-10-24 19:26:00 +0700

    edit-news/page

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 import Navbar from "../components/News/Navbar"
 import Content from "../components/News/Content"
 
+const LoginContainer = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+  background-color: #bf7f4bc9;
+`
+
 function News() {
   return (
-    <div className='login-container'>
+    <LoginContainer>
       <Navbar />
       <Content />
-    </div>
+    </LoginContainer>
   );
 }
 export default News
