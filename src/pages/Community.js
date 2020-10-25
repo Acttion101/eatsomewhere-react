e9ef9f73dab9219935ec02325397b56cd45a1a94
Acttion101 <pongponpan102@gmail.com>
2020-10-26 02:45:39 +0700

    new-Community.js

---
+++ src/pages/Community.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from "../components/Community/Navbar"
+import Content from "../components/Community/Content"
+
+const LoginContainer = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+  background-color: #bf7f4bc9;
+`
+
+function Community() {
+  return (
+    <LoginContainer>
+      <Navbar />
+      <Content />
+    </LoginContainer>
+  );
+}
+export default Community
