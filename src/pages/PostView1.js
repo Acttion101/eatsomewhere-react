77e3a0c252e151f57b073263ba8846c4804dec13
Acttion101 <pongponpan102@gmail.com>
2020-10-26 03:22:21 +0700

    new-PostView/Page

---
+++ src/pages/PostView1.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from "../components/PostView1/Navbar"
+import Content from "../components/PostView1/Content"
+
+const LoginContainer = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+  background-color: #bf7f4bc9;
+`
+
+function PostView() {
+  return (
+    <LoginContainer>
+      <Navbar />
+      <Content />
+    </LoginContainer>
+  );
+}
+export default PostView
