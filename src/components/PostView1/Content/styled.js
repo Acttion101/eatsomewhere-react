60d0a59bed99b225299014695dc79202f3889028
Acttion101 <pongponpan102@gmail.com>
2020-10-26 03:21:22 +0700

    new-PostView/styled

---
+++ src/components/PostView1/Content/styled.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const BackContent = styled.div`
+  display: flex;
+  margin: 50px 0 0 50px;
+  padding: 0 10px 10px;
+  border-radius: 2rem;
+  align-items: top;
+  height: 100vh;
+  width: 70vw;
+  background-color: #ffffff;
+  flex-direction: column;
+  align-items: center;
+  input{
+    width: 600px;
+    height:50px;
+    display:flex;
+    flex-direction:column;
+    margin: 10px;
+  }
+  button{
+    
+  }
+`
+export const Container = styled.div`
+  padding: 2px 16px;
+
+`
+export const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  width: 1000px;
+  height: 200px;
+  
+
+`
