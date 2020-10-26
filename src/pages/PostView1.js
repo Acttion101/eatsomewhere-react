f001f22553fda741d7b0e476ef006f6c4d35adf2
Acttion101 <pongponpan102@gmail.com>
2020-10-26 21:48:30 +0700

    edit-PostView

--- src/pages/PostView1.js
+++ src/pages/PostView1.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
-import Navbar from "../components/PostView1/Navbar"
-import Content from "../components/PostView1/Content"
+import Navbar from '../components/PostView1/Navbar'
+import Content from '../components/PostView1/Content'
 
 const LoginContainer = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  flex-direction:column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   background-color: #bf7f4bc9;
 `
 
@@ -17,6 +17,6 @@ function PostView() {
       <Navbar />
       <Content />
     </LoginContainer>
-  );
+  )
 }
 export default PostView
