66f19f4bc990d2e9c089585fe5ca836dfbdc8865
ramita-kahapana <ramitakahapana@gmail.com>
2020-10-26 20:10:48 +0700

    edit-styled/footer/home/login/register

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,18 +1,12 @@
 import styled from 'styled-components'
 
-
 export const Footer = styled.footer`
   background-color: #FF7700;
   display: flex;
   justify-content: space-between;
   padding: 10px;
-  height: 60px;
+  height: auto;
+  width:100vw;
 
  
 `
-
-
-
-
-
-
