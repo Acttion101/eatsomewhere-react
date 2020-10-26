05022e794d05e7487d272d27133bd278a3158886
Acttion101 <pongponpan102@gmail.com>
2020-10-27 01:28:04 +0700

    edit-PostView/Page

---
+++ src/pages/PostView2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import TodoFrom from '../components/Todo/TodoFrom'
+import TodoList from '../components/Todo/TodoList'
+
+export default function PostView2 () {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  )
+}
